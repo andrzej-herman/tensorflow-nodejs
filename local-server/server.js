46f3a895646ea0e7a3d792d6af642281f49de750
andrzejherman <andrzej.herman@r.pl>
2019-10-21 15:58:27 +0200

    Ustawienie CORS dla modelu.

--- local-server/server.js
+++ local-server/server.js
@@ -6,7 +6,13 @@ let app = express();
 
 // loguję w konsoli przychodzące requesty (data, metoda, adres url requestu)
 app.use(function(req, res, next) {
+	// loguję w konsoli przychodzące requesty (data, metoda, adres url requestu)
 	console.log(`${new Date()} - ${req.method} request for ${req.url}`);
+	// ustawiam CORS => zezwalam na dostęp do modelu z zewnętrznych adresów
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	res.setHeader('Access-Control-Allow-Credentials', true);
 	next();
 });
 
--- static/predict_image.js
+++ static/predict_image.js
@@ -5,23 +5,48 @@
 async function loadModel() {
 	$('#model-loader').show();
 	model = undefined;
-	// model = await tf.loadModel(`http://136.243.117.158:3001/tfjs-models/MobileNet/model.json`);
-	model = await tf.loadModel(`http://localhost:3001/tfjs-models/MobileNet/model.json`);
+	// => ładowanie modelu MobileNet z pliku model.json zawierającego wskaźniki do plików wag
+	// => model na serwerze
+	model = await tf.loadModel(`http://136.243.117.158:3001/tfjs-models/MobileNet/model.json`);
+	// => model lokalnie
+	// => model = await tf.loadModel(`http://localhost:3001/tfjs-models/MobileNet/model.json`);
 	$('#model-loader').hide();
 	document.getElementById('predict-button').disabled = true;
 }
 
+// => funkcja optymalizująca obraz do wymaganego przez model MobileNet
 function preprocessImage(image) {
+	// => tworzę tensor z pikseli obrazu używając funkcji TF 'fromPixels'
+	// => ustawiam rozmiar tensora na 224 x 224 - wymagany przez model MobileNet
+	// => konwertuję jego wartości na typ float
 	let tensor = tf.fromPixels(image).resizeNearestNeighbor([ 224, 224 ]).toFloat();
+	// => Optymalizacja wag
+	// => uśredniam wartości R, G, B
+	// -> zwracam tensor zoptymalizowany (różnica tensora przed optymalizacją i tensora ze średnią wartością (0-255)
+	// podzielonego dodatkowo przez tensor uśredniony)
 	let offset = tf.scalar(127.5);
 	return tensor.sub(offset).div(offset).expandDims();
 }
 
 async function runImageRecognition() {
+	// => pobieram obraz z pliku
 	let image = $('#selected-image').get(0);
+	// => wywołuję funkcję optymalizującą tensor wejściowy
 	let tensor = preprocessImage(image);
 	var m = model;
+	// => rozpoczynam predykcję wyników przekazując przygotowany tensor do funkcji TF predict
+	// => zwraca on tensor wyjściowy (wyniki predykcji) na podstawie tensora wyjściowego (input)
+	// => na tensorze wyjściowym wykonuję metodę 'data', która asynchronicznie ładuje otrzymane wartości z tensora do tablicy
 	let predictions = await model.predict(tensor).data();
+	// => tworzę listę 5 najbardziej prawdopodobnych wyników
+	// 	z tablicy predykcji tworzę tablicę obiektów,
+	// 	każdy z obiektów zawiera wartość prawdopodobieństwa co przedstawia obrazek
+	// 	oraz nazwę klasy z IMAGENET => de facto AI zwraca jako output prawdopodobieństwo, że
+	// 	obraz przedstawia taki numer klasy IMAGE_NET, oraz numer klasy - a ten numer klasy oznacza nazwę obiektu
+	// 	który przedstawia obraz => pobieram zatem tę nazwę z pliku img_classes_pl.js
+	// 	dodatkowo do kdego obiektu dodaję wartość procentową prawdopodobieństwa
+	// 	=> następnie sortuję obiekty po prawdopodobieństwie malejąco
+	// 	i pobieram pierwsze pięć najwyższe wyniki
 	let top5 = Array.from(predictions)
 		.map(function(p, i) {
 			return {
@@ -39,6 +64,7 @@ async function runImageRecognition() {
 	$('#pred-loader').hide();
 }
 
+// => formatowanie wyświetlania wyników
 function getDisplayName(name) {
 	var commas = name.split(',').length - 1;
 	if (commas <= 1) {
@@ -49,6 +75,7 @@ function getDisplayName(name) {
 	}
 }
 
+// => wyświetlanie wyników predykcji w progres barach
 function showResults(results) {
 	var name1 = document.getElementById('res-name-01');
 	var progress1 = document.getElementById('res-progress-01');
@@ -88,29 +115,41 @@ function showResults(results) {
 	$('#results-set').show();
 }
 
+// => model AI
 let model;
+// => wywołanie ładowania modelu (pliku wag model.json)
 loadModel();
 
+// => wybieranie obrazu z dysku
 $('#image-selector').change(function() {
+	// => tworzenie obiektu reader do wczytania zawartości obrazu
 	let reader = new FileReader();
 	reader.onload = function() {
 		let dataURL = reader.result;
+		// => wyświetlanie wybranego obrazu na stronie
 		$('#selected-image').attr('src', dataURL);
 	};
+	// => wybranie pierwszego zaznaczonego obrazu w oknie wyboru
 	let file = $('#image-selector').prop('files')[0];
+	// => czytanie zawartości obrazu
 	reader.readAsDataURL(file);
+	// => informacja dla Użytkownika, że poprawnie wybrano obraz
 	var titleBig = document.getElementById('image-set');
 	titleBig.innerHTML = 'Wybrano obraz';
 	var titleSmall = document.getElementById('image-filename');
 	titleSmall.innerHTML = 'Nazwa pliku: ' + file.name;
+	// => uaktywnienie przycisku "Rozpoznaj obraz"
 	document.getElementById('predict-button').disabled = false;
 });
 
+// => obsłużenie przycisku "Rozpoznaj obraz" => uruchomienie procesu rozpoznawania obrazu przez AI
 $('#predict-button').click(function() {
+	// ładowanie "czekacza"
 	$('#pred-loader').show();
 	runImageRecognition();
 });
 
+// => czyszczenie formularza (przycisk reset)
 $('#reset-button').click(function() {
 	document.getElementById('image-selector').value = '';
 	document.getElementById('image-set').innerHTML = 'Brak wybranego obrazu do rozpoznania';
@@ -119,114 +158,3 @@ $('#reset-button').click(function() {
 	document.getElementById('predict-button').disabled = true;
 	$('#results-set').hide();
 });
-
-// ---------------------------------------------------------------------
-// STARA WERSJA
-// // => wybieranie obrazu z dysku
-// $('#image-selector').change(function() {
-// 	// => tworzenie obiektu reader do wczytania zawartości obrazu
-// 	let reader = new FileReader();
-// 	reader.onload = function() {
-// 		let dataUrl = reader.result;
-// 		// => wyświetlanie wybranego obrazu na stronie
-// 		$('#selected-image').attr('src', dataUrl);
-// 		// => czyszczenie listy predykcji
-// 		$('#prediction-list').empty();
-// 	};
-
-// 	// => wybranie pierwszego zaznaczonego obrazu w oknie wyboru
-// 	let file = $('#image-selector').prop('files')[0];
-// 	// => czytanie zawartości obrazu
-// 	reader.readAsDataURL(file);
-// });
-
-// // => model AI
-// let model;
-// (async function() {
-// 	// => ładowanie modelu VGG19 z pliku model.json zawierającego wskaźniki do plików wag
-// 	model = await tf.loadModel('http://localhost:1973/tfjs-models/VGG19/model.json');
-// 	$('.herman-loader').hide();
-// })();
-
-// async function runNeuralNetwork() {
-// 	// => pobieram obraz z pliku
-// 	let image = $('#selected-image').get(0);
-// 	// => tworzę tensor z pikseli obrazu używając funkcji TF 'fromPixels'
-// 	// => ustawiam rozmiar tensora na 224 x 224 - wymagany przez model VGG19
-// 	// => konwertuję jego wartości na typ float
-
-// 	// => WERSJA TENSORA PRZED OPTYMALIZACJĄ WAG
-// 	//let tensor = tf.fromPixels(image).resizeNearestNeighbor([ 224, 224 ]).toFloat().expandDims();
-
-// 	// PRZYGOTOWANA DO OPTTYMALIZACJI (bez rozszerzania - nastepi ono po optymalizacji wag tensora)
-// 	let tensor = tf.fromPixels(image).resizeNearestNeighbor([ 224, 224 ]).toFloat();
-
-// 	// ------------------------------------------------------------------------
-// 	// => Optymalizacja wag
-// 	// Od każdego pixela o kolorze R, G lub B odejmuje średnią dla tego koloru
-// 	let meanImageNetRGB = {
-// 		red: 123.68,
-// 		green: 116.779,
-// 		blue: 103.939
-// 	};
-
-// 	// => buduję tablicę jednowymiarowych tensorów
-// 	let indices = [ tf.tensor1d([ 0 ], 'int32'), tf.tensor1d([ 1 ], 'int32'), tf.tensor1d([ 2 ], 'int32') ];
-
-// 	// => uśredniam wartości Kolorów R, G, B, odejmując od wartości koloru (0-255) wartość średnią z ImageNet
-// 	// tworzę obiekt zawierający tensory dla każdego uśrednionego koloru
-// 	let centeredRGB = {
-// 		red: tf.gather(tensor, indices[0], 2).sub(tf.scalar(meanImageNetRGB.red)).reshape([ 50176 ]),
-// 		green: tf.gather(tensor, indices[1], 2).sub(tf.scalar(meanImageNetRGB.green)).reshape([ 50176 ]),
-// 		blue: tf.gather(tensor, indices[2], 2).sub(tf.scalar(meanImageNetRGB.blue)).reshape([ 50176 ])
-// 	};
-
-// 	let optimizedTensor = tf
-// 		.stack([ centeredRGB.red, centeredRGB.green, centeredRGB.blue ], 1)
-// 		.reshape([ 224, 224, 3 ])
-// 		.reverse(2)
-// 		.expandDims();
-
-// 	// ------------------------------------------------------------------------
-
-// 	// => rozpoczynam predykcję wyników przekazując przygotowany tensor do funkcji TF predict
-// 	// => zwraca on tensor wyjściowy (wyniki predykcji) na podstawie tensora wyjściowego (input)
-// 	// => na tensorze wyjściowym wykonuję metodę 'data', która asynchronicznie ładuje otrzymane wartości z tensora do tablicy
-// 	let predictions = await model.predict(optimizedTensor).data();
-// 	// => tworzę listę 5 najbardziej prawdopodobnych wyników
-// 	// z tablicy predykcji tworzę tablicę obiektów,
-// 	// każdy z obiektów zawiera wartość prawdopodobieństwa co przedstawia obrazek
-// 	// oraz nazwę klasy z IMAGENET => de facto AI zwraca jako output prawdopodobieństwo, że
-// 	// obraz przedstawia taki numer klasy IMAGE_NET, oraz numer klasy - a ten numer klasy oznacza nazwę obiektu
-// 	// który przedstawia obraz => pobieram zatem tę nazwę z pliku imagenet_classes.js
-// 	// dodatkowo do kdego obiektu dodaję wartość procentową prawdopodobieństwa
-// 	// => następnie sortuję obiekty po prawdopodobieństwie malejąco
-// 	// i pobieram pierwsze trzy najwyższe wyniki
-// 	let top3 = Array.from(predictions)
-// 		.map(function(p, idx) {
-// 			return {
-// 				probability: p,
-// 				percentage: (p * 100).toFixed(3).toString() + ' %',
-// 				className: IMAGENET_CLASSES[idx]
-// 			};
-// 		})
-// 		.sort(function(a, b) {
-// 			return b.probability - a.probability;
-// 		})
-// 		.slice(0, 5);
-
-// 	// => czyszczę listę predykcji po poprzednim obrazie
-// 	$('#prediction-list').empty();
-// 	// => i wyświetlam w niej nowe wyniki
-// 	top3.forEach(function(item) {
-// 		$('#prediction-list').append(`<li>${item.className}: ${item.percentage}</li>`);
-// 	});
-
-// 	predictionImg.style.visibility = 'hidden';
-// }
-
-// // => obsłużenie przycisku "Rozpoznaj obraz" => uruchomienie procesu rozpoznawania obrazu przez AI
-// $('#predict-button').click(function() {
-// 	predictionImg.style.visibility = 'visible';
-// 	runNeuralNetwork();
-// });
